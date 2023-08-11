064e70c602ebdc0032daf2460f517253de74a72c
ricci2511 <ricardo.christmann@protonmail.com>
2023-08-11 17:59:13 +0200

    dedupsc: refactor custom key generator tests

--- dedupsc/movie-tv-key-generator_test.go
+++ dedupsc/movie-tv-key-generator_test.go
@@ -2,93 +2,33 @@ package main
 
 import "testing"
 
-func TestMovieFileName(t *testing.T) {
-	path := "Alien - 1979 - Bluray-1080p.mkv"
-
-	movieKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
+func TestMovieTvFileNamesKeyGenerator(t *testing.T) {
+	tcs := []struct {
+		path     string
+		expected string
+	}{
+		{"Alien - 1979 - Bluray-1080p.mkv", "Alien1979"},
+		{"Alien.1979.1080p.BluRay.x265-RelGroup.mp4", "Alien1979"},
+		{"8 Mile - 2002 - Bluray-1080p.mkv", "8Mile2002"},
+		{"8.Mile.2002.1080p.BluRay.x265-RelGroup.mp4", "8Mile2002"},
+		{"Breaking Bad - S01E01 - Pilot.mkv", "BreakingBadS01E01"},
+		{"Breaking Bad - 1x01 - Pilot.mkv", "BreakingBad1x01"},
+		{"Breaking.Bad.S01E01.1080p.BluRay.x265-RelGroup.mkv", "BreakingBadS01E01"},
+		{"Star Trek Deep Space Nine - S01E01-E02 - Emissary.mkv", "StarTrekDeepSpaceNineS01E01E02"},
+		{"Star Trek Deep Space Nine - 1x01-02 - Emissary.mkv", "StarTrekDeepSpaceNine1x0102"},
+		{"Breaking.Bad.S01E01.1080p.BluRay.x265-RelGroup.mp3", ""}, // Unsupported file extension returns empty key
 	}
 
-	if movieKey != "Alien1979" {
-		t.Errorf("Expected key to be 'Alien1979', got '%s'", movieKey)
-	}
-}
+	for _, tc := range tcs {
+		t.Run(tc.path, func(t *testing.T) {
+			key, err := movieTvFileNamesKeyGenerator(tc.path)
+			if err != nil {
+				t.Error(err)
+			}
 
-func TestMovieFileNameReleaseGroup(t *testing.T) {
-	path := "Alien.1979.1080p.BluRay.x265-RelGroup.mkv"
-
-	movieKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if movieKey != "Alien1979" {
-		t.Errorf("Expected key to be 'Alien1979', got '%s'", movieKey)
-	}
-}
-
-func TestTvFileName(t *testing.T) {
-	path := "Breaking Bad - S01E01 - Pilot.mkv"
-
-	tvKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if tvKey != "BreakingBadS01E01" {
-		t.Errorf("Expected key to be 'BreakingBadS01E01', got '%s'", tvKey)
-	}
-}
-
-func TestTvFileNameReleaseGroup(t *testing.T) {
-	path := "Breaking.Bad.S01E01.1080p.BluRay.x265-RelGroup.mkv"
-
-	tvKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if tvKey != "BreakingBadS01E01" {
-		t.Errorf("Expected key to be 'BreakingBadS01E01', got '%s'", tvKey)
-	}
-}
-
-func TestTvMultiEpisodeFileName(t *testing.T) {
-	path := "Star Trek Deep Space Nine - S01E01-E02 - Emissary.mkv"
-
-	tvKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if tvKey != "StarTrekDeepSpaceNineS01E01E02" {
-		t.Errorf("Expected key to be 'StarTrekDeepSpaceNineS01E01E02', got '%s'", tvKey)
-	}
-}
-
-func TestAlternateTvFileNamePattern(t *testing.T) {
-	path := "Breaking Bad - 1x01 - Pilot.mkv"
-
-	tvKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if tvKey != "BreakingBad1x01" {
-		t.Errorf("Expected key to be 'BreakingBad1x01', got '%s'", tvKey)
-	}
-}
-
-func TestInvalidFileExtension(t *testing.T) {
-	path := "Breaking.Bad.S01E01.1080p.BluRay.x265-RelGroup.mp3"
-
-	tvKey, err := movieTvFileNamesKeyGenerator(path)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if tvKey != "" {
-		t.Errorf("Expected key to be '', got '%s'", tvKey)
+			if key != tc.expected {
+				t.Errorf("Expected key to be '%s', got '%s'", tc.expected, key)
+			}
+		})
 	}
 }
